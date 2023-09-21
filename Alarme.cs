a1e921451e20561a28209a64508bfbff585f6a40
slarrie <slarrie@hotmail.com>
2023-09-21 12:48:33 -0300

    Add files via upload
    A hora do Pitch

---
+++ Alarme.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AHoraDoPitch
+{
+    public class Alarme
+    {
+        public Alarme()
+        {
+            this.Tempo = 3; //três minutos
+        }
+
+        public Alarme(int tempo)
+        {
+            this.Tempo = tempo;
+        }
+
+        //Controla do tempo do Pitch
+        private int tempo;
+
+        public int Tempo
+        {
+            get { return tempo; }
+            set { tempo = 1000 * 60 * value; } //Calculo para converter segundo em minuto.
+        }
+
+        public void Iniciar()
+        {
+            Thread.Sleep(this.Tempo);
+            Console.Beep(400,2000);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHoraDoPitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarme alarme = new Alarme();
+            String resposta = "S";
+
+            while (resposta != "N") 
+            {
+                Console.Clear();
+                Console.WriteLine("A hora do Pitch");
+                Console.Write("Informe o minuto do pitch: ");
+                int tempo = Convert.ToInt32(Console.ReadLine());
+                alarme.Tempo = tempo;
+                Console.WriteLine("Tempo iniciado...");
+                alarme.Iniciar();
+                Console.WriteLine("O seu tempo acabou!!!");
+                Console.WriteLine("Deseja executar o programa novamente? S/N");
+                resposta = Console.ReadLine().ToUpper();
+            }
+        }
+    }
+}
